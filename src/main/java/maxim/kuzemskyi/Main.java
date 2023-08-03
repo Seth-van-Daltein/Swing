25c8f2be14d3d3bda093088ab8bde119b7773fd8
Seth-van-Daltein <sethvandalstein@gmail.com>
2023-08-04 00:34:25 +0300

    actions

--- src/main/java/maxim/kuzemskyi/Main.java
+++ src/main/java/maxim/kuzemskyi/Main.java
@@ -5,59 +5,32 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Main {
+
+    static JFrame jFrame = getFrame();
+    static JPanel jPanel = new JPanel();
     public static void main(String[] args) {
-        JFrame jFrame = getFrame();
+        jFrame.add(jPanel);
 
-        JPanel jPanelCloseOperation = new JPanel();
-        jFrame.add(jPanelCloseOperation);
+        JButton jButton = new JButton(new MyAction());
+        jButton.setText("gray");
+        jPanel.add(jButton);
 
-        JButton jButtonCCC = new JButton("close");
-        jPanelCloseOperation.add(jButtonCCC);
-
-        jButtonCCC.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JFrame jFrameClose = new JFrame();
-                jFrameClose.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-                //Робимо для того щоб вікно виводилось посередині екрану незалежно від розширення екрану
-                Toolkit toolkit = Toolkit.getDefaultToolkit();
-                Dimension dimension = toolkit.getScreenSize();
-                jFrameClose.setBounds(dimension.width / 2 - 200, dimension.height / 2 - 150, 300, 200); //розташування вікна рівно по центру
-
-                //Change title(name) of the Window
-                jFrameClose.setTitle("Close Window");
-
-                //Робимо вікно видимим
-                jFrameClose.setVisible(true);
+        KeyStroke keyStroke = KeyStroke.getKeyStroke("ctrl B");
+        InputMap inputMap = jPanel.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        inputMap.put(keyStroke, "changeColor");
+        ActionMap actionMap = jPanel.getActionMap();
+        actionMap.put("changeColor", new MyAction());
 
 
+    }
 
-                JPanel jPanelClose = new JPanel();
-                jFrameClose.add(jPanelClose);
 
-                JButton jButtonCloseOk = new JButton("Ok");
-                JButton jButtonCloseCancel = new JButton("Cancel");
+    static class MyAction extends AbstractAction{
 
-                jPanelClose.add(jButtonCloseOk);
-                jPanelClose.add(jButtonCloseCancel);
-
-                jButtonCloseOk.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        jFrameClose.setVisible(true);
-                        System.exit(0);
-                    }
-                });
-
-                jButtonCloseCancel.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        jFrameClose.setVisible(false);
-                    }
-                });
-            }
-        });
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            jPanel.setBackground(Color.gray);
+        }
     }
 
 
